0c88e240a7215ff6a89d3f6ac59eaeb02b713811
albertorodriguez <arodr243@calpoly.edu>
2019-04-14 17:15:57 -0700

    deleting client and server files

--- src/server/test/server.test.js
+++
@@ -1,9 +0,0 @@
-
-const request = require("supertest") ; 
-const app = require("../server") ; 
-
-describe("Smoke Test: /api/test", () => {
-	it("200 status code", (done) => {
-		request(app).get("/api/test").expect(200, done) ; 
-	}) ; 
-}) ; 
